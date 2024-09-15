a31ac4d31fbcf82cc357d802787bdd238ac90b61
Piyal_Madushanka <piyalmadushankaofficial@gmail.com>
2024-09-15 20:51:52 +0530

    New tab navigation added

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,26 +1,47 @@
 package org.example.project
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.style.TextOverflow
-import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.Tab
-import cafe.adriel.voyager.transitions.SlideTransition
-import org.example.project.screen.home.HomeScreen
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import org.example.project.tab.home.HomeTab
+import org.example.project.tab.profile.ProfileTab
+import org.example.project.tab.setting.SettingTab
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        Navigator(HomeScreen()) { navigator: Navigator ->
-            SlideTransition(navigator)
+        TabNavigator(HomeTab) {
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation {
+                        TabNavigationItem(HomeTab)
+                        TabNavigationItem(ProfileTab)
+                        TabNavigationItem(SettingTab)
+                    }
+                },
+                content = {
+                    CurrentTab()
+                }
+            )
         }
+
+
+//        Navigator(HomeScreen()) { navigator: Navigator ->
+//
+////            SlideTransition(navigator)
+//        }
     }
 }
 
