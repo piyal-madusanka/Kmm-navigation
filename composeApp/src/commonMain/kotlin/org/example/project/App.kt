f6a43009602e4b6a05fd868097290d492ae63b7f
Piyal_Madushanka <piyalmadushankaofficial@gmail.com>
2024-09-15 20:46:21 +0530

    New tab navigation extension added

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,8 +1,15 @@
 package org.example.project
 
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.transitions.SlideTransition
 import org.example.project.screen.home.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -11,8 +18,33 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        Navigator(HomeScreen()){navigator: Navigator ->
+        Navigator(HomeScreen()) { navigator: Navigator ->
             SlideTransition(navigator)
         }
     }
+}
+
+@Composable
+private fun RowScope.TabNavigationItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+
+    BottomNavigationItem(
+        selected = tabNavigator.current == tab,
+        onClick = { tabNavigator.current = tab },
+        label = {
+            Text(
+                text = tab.options.title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        icon = {
+            tab.options.icon?.let {
+                Icon(
+                    painter = it,
+                    contentDescription = tab.options.title
+                )
+            }
+        }
+    )
 }
